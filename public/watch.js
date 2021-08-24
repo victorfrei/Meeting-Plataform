d723601a89f7db4dd49781d62542c6bec4d8fc55
Victor Freire <vic.frei2@gmail.com>
2021-08-24 21:19:36 +0000

    v1.0.6-v2 webrtc

--- public/watch.js
+++ public/watch.js
@@ -2,7 +2,25 @@ let peerConnection;
 const config = {
   iceServers: [
     {
-      urls: ["stun:stun.l.google.com:19302"]
+      urls: ["stun:stun.l.google.com:19302",
+        "stun1.l.google.com:19302",
+        "stun2.l.google.com:19302",
+        "stun3.l.google.com:19302",
+        "stun4.l.google.com:19302",
+        "stun01.sipphone.com",
+        "stun.ekiga.net",
+        "stun.fwdnet.net",
+        "stun.ideasip.com",
+        "stun.iptel.org",
+        "stun.rixtelecom.se",
+        "stun.schlund.de",
+        "stunserver.org",
+        "stun.softjoys.com",
+        "stun.voiparound.com",
+        "stun.voipbuster.com",
+        "stun.voipstunt.com",
+        "stun.voxgratia.org",
+        "stun.xten.com"]
     }
   ]
 };
@@ -11,39 +29,39 @@ const socket = io.connect();
 const video = document.querySelector("video");
 
 socket.on("offer", (id, description) => {
-    peerConnection = new RTCPeerConnection(config);
-    peerConnection
-      .setRemoteDescription(description)
-      .then(() => peerConnection.createAnswer())
-      .then(sdp => peerConnection.setLocalDescription(sdp))
-      .then(() => {
-        socket.emit("answer", id, peerConnection.localDescription);
-      });
-    peerConnection.ontrack = event => {
-      video.srcObject = event.streams[0];
-    };
-    peerConnection.onicecandidate = event => {
-      if (event.candidate) {
-        socket.emit("candidate", id, event.candidate);
-      }
-    };
-  });
-
-  socket.on("candidate", (id, candidate) => {
-    peerConnection
-      .addIceCandidate(new RTCIceCandidate(candidate))
-      .catch(e => console.error(e));
-  });
-  
-  socket.on("connect", () => {
-    socket.emit("watcher");
-  });
-  
-  socket.on("broadcaster", () => {
-    socket.emit("watcher");
-  });
-  
-  window.onunload = window.onbeforeunload = () => {
-    socket.close();
-    peerConnection.close();
+  peerConnection = new RTCPeerConnection(config);
+  peerConnection
+    .setRemoteDescription(description)
+    .then(() => peerConnection.createAnswer())
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(() => {
+      socket.emit("answer", id, peerConnection.localDescription);
+    });
+  peerConnection.ontrack = event => {
+    video.srcObject = event.streams[0];
   };
+  peerConnection.onicecandidate = event => {
+    if (event.candidate) {
+      socket.emit("candidate", id, event.candidate);
+    }
+  };
+});
+
+socket.on("candidate", (id, candidate) => {
+  peerConnection
+    .addIceCandidate(new RTCIceCandidate(candidate))
+    .catch(e => console.error(e));
+});
+
+socket.on("connect", () => {
+  socket.emit("watcher");
+});
+
+socket.on("broadcaster", () => {
+  socket.emit("watcher");
+});
+
+window.onunload = window.onbeforeunload = () => {
+  socket.close();
+  peerConnection.close();
+};

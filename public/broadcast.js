e1983fb549a25b7c6dfcd8433ec24096d9417db8
victorfrei <vic.frei2@gmail.com>
2021-08-24 21:44:46 -0300

    v1.0.4

--- public/broadcast.js
+++ public/broadcast.js
@@ -3,10 +3,6 @@ const config = {
   iceServers: [
     {
       urls: ["stun:stun.l.google.com:19302",
-      "stun1.l.google.com:19302",
-      "stun2.l.google.com:19302",
-      "stun3.l.google.com:19302",
-      "stun4.l.google.com:19302",
       "stun01.sipphone.com",
       "stun.ekiga.net",
       "stun.fwdnet.net",
